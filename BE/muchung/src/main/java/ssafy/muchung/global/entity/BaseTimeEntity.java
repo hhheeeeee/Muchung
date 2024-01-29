ad085f7369c519213aaa6afa331b7e45d6284432
소재열 <devjy0309@gmail.com>
2024-01-29 16:13:46 +0900

    fix: import 오타 수정
    Former-commit-id: 7d3ac6bbbadfe416cffb3478e97916c56dfe6c65

--- BE/muchung/src/main/java/ssafy/muchung/global/entity/BaseTimeEntity.java
+++ BE/muchung/src/main/java/ssafy/muchung/global/entity/BaseTimeEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d36a08bcb91878fcbb2aad51ce24503fa0f3d02ef88286f75364bf95e7546b8b
-size 654
+oid sha256:e64ed9fe91cec116fe58a9be6fdcbeb69af47325958af7e8acf487a8f4ed19c6
+size 708
