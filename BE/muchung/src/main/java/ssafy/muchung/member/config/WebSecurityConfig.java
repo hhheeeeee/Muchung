a1a62e25f8051d74a2d62f41e4c1f61103fae8f4
이수현 <olafminion0116@naver.com>
2024-02-01 22:15:23 +0900

    chore: SpringSecurity 필터 무시 URI 패턴으로 /hello 관련 추가
    Former-commit-id: 73489e61eb9e1c76e4e7098531876f0f31d277fa

--- BE/muchung/src/main/java/ssafy/muchung/member/config/WebSecurityConfig.java
+++ BE/muchung/src/main/java/ssafy/muchung/member/config/WebSecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec85bdfb6b55007584ee5d47d17f52a8c36a7743bb096746cc64f4866aea4a64
-size 2144
+oid sha256:0e0875d0d69c52972d411a4c040107f30da6a385c5630927d9a5e9856ee331c0
+size 2174
