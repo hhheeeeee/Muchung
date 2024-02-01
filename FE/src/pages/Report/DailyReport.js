198559ac31c70187ae4bbc2ba69c55a7271c6530
KimTaeHun <kth32105@naver.com>
2024-02-01 19:02:30 +0900

    [FIX] FE : push test
    Former-commit-id: caa4ef6820f5f5a82240f86fcad4244053d66fd5

--- FE/src/pages/Report/DailyReport.js
+++ FE/src/pages/Report/DailyReport.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:729fc19484cb8fb4765ce19ec5e81a38b54f54381463f3575e78687e3a5c3d9e
-size 6824
+oid sha256:e289df4efd4dae529455b1afee6f39a92b540866a03f56f5a3acd633ccf05102
+size 6846
