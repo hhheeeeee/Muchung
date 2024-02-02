3850695716ed4e7bdd211ae98f1bf4ee89743fbf
Kim Taehun <kth32105@naver.com>
2024-02-03 01:47:19 +0900

    [feat] FE : 메인 화면 영역 분할 완료
    Former-commit-id: 53e672f1e02beb7ab04d67a05f341547468795cb

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe6fe3c5557de94d48ff0c13fd60ebe1256e17dc576639b303cc2ff70ca7f2ee
-size 2112
+oid sha256:583c1e06f61014cdb9a599387429d825f2fa06cef56d1acf0c84ae86ff85a86b
+size 2203
--- FE/src/pages/MainPage/MainPage.js
+++ FE/src/pages/MainPage/MainPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15be27dc0fdf1bac9b4cdf22f4a3e35b0e6abcb4a5a746c4149286eb7416f3bc
-size 2290
+oid sha256:284cd2e0c36eadff9e7feea90e6e1cd65aaf1a8aec7658b9814f7c5fa9a4c95a
+size 2119
