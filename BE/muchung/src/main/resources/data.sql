3d5ae13362fc9fd1e83cee3ae85975f53d388742
소재열 <devjy0309@gmail.com>
2024-02-02 18:04:46 +0900

    feat: 더미데이터 추가
    Former-commit-id: dc8380e028e08d82250c43bd92c5e33af6dee985

--- BE/muchung/src/main/resources/data.sql
+++ BE/muchung/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6d9c78f40c106ec3a1df93806ef9b3c6abf808d3be91620d615e6af5ddb312b
-size 832
+oid sha256:41fc9c44217fc5f65ad158fb99762e80d82d52b577de7d9c5d9fd662baa50af5
+size 1497
