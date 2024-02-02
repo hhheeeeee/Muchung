bf64f9650d6529199813e2c76e2b6378038120be
소재열 <devjy0309@gmail.com>
2024-02-02 10:10:45 +0900

    feat: 페이징 request 파라미터 객체 추가
    Former-commit-id: 4d8d29275689335cf3c56d5c28c52c90b1cbb033

---
+++ BE/muchung/src/main/java/ssafy/muchung/report/dto/request/PagingCondition.java
@@ -0,0 +1,18 @@
+package ssafy.muchung.report.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class PagingCondition {
+	/*  반환 size - 필수
+	 * */
+	private Long limit;
+
+	/*  페이지 번호 - option - default 0
+	 * */
+	private long pageNumber;
+}
