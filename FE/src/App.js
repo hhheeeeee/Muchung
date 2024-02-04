5de3e1a6dc127f1232313b91801822257bea3caf
hhheeeeee <libravickyyy@gmail.com>
2024-02-04 18:34:35 +0900

    [feat] FE : navbar 수정
    Former-commit-id: 6dc696dfdad8419613d966787b73e71051184a27

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50e8996932f0a81d06245656e9e11bb87deafda8fc09b17a05102ccea10f7b0c
-size 2172
+oid sha256:43cf71a0eebee113af815a9ea1fd8834e7da5f12b651bba1d23908466ff38289
+size 2355
--- FE/src/pages/MainPage/NavMenus.js
+++ FE/src/pages/MainPage/NavMenus.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89799070780780be260e44c407d2cc75e28fd2d83a0fa1313ba5da83a35c0210
-size 2204
+oid sha256:00471bb2e5bae2db3aa89406c5940b358384e778ee540f90ff8d9527f61586e7
+size 5323
