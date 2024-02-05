b2511693f681a215435b1c52214903be396b3dd8
김이현 <rihyun1126@naver.com>
2024-02-05 20:26:32 +0900

    fix: JWT 오류 처리 Former-commit-id: c4b0cdff976ead98753c180027fb32f0dcb52cdb

--- BE/muchung/src/main/java/ssafy/muchung/member/controller/TokenController.java
+++ BE/muchung/src/main/java/ssafy/muchung/member/controller/TokenController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f043dd4c3739d6207a51a32e14ecfedd707649d118162eff793e034f2675baf
-size 1001
+oid sha256:137028572bf4854f3c981b53a41700fe9ea82b8dd89b5650469be20ab06ea068
+size 1002
