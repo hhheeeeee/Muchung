64524f6cdc39044581cc849a7bfd329686c4519f
김승희 <libravickyyy@gmail.com>
2024-02-05 17:52:32 +0900

    Revert "[feat] FE : 받은이메일 표 추가"
    This reverts commit 87ddd0d49669939fb868870dd1b1722a44e99eeb [formerly 318f980fafac953a002aa090c645a5b46a74c21c].
    
    
    Former-commit-id: c57ee2c6a05bce32bea97f50bac81fae9630028f

--- FE/src/pages/Email/RecievedEmail.js
+++ FE/src/pages/Email/RecievedEmail.js
@@ -1,3 +1,19 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cb3cb381481526f83c5435df8348c981869bc2bceb4ee25a379ff4087739a778
-size 1755
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { Outlet } from "react-router-dom";
+
+const RecievedEmail = () => {
+  return <div css={container}>받은거~</div>;
+};
+
+export default RecievedEmail;
+
+const container = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+  background-color: pink;
+`;
