eb83b6b8507ef1736c282b94cb8c1f0135772e7e
이수현 <olafminion0116@naver.com>
2024-02-06 10:12:56 +0900

    chore: URI 패턴 변경
    Former-commit-id: 74070c30adba2e08ee30d900a99247e5f493c0cc

--- BE/muchung/src/main/java/ssafy/muchung/global/config/WebSecurityConfig.java
+++ BE/muchung/src/main/java/ssafy/muchung/global/config/WebSecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d45dca75db113470aedba851fd4269dc3c0f1a56aeb7c305575e877308bea72
-size 2236
+oid sha256:c5babd2ecdd4ac477bb200cbb19d0970b1d69af7ebf499677feae5c5e4027855
+size 2251
