642048851c31a5c8b6945318e1dfedb4972271c9
소재열 <devjy0309@gmail.com>
2024-02-06 17:52:29 +0900

    좋아요 기능 개발
    Former-commit-id: ccbe0ab21d7330929c6593b1649453eb9f1b237c

--- BE/muchung/src/main/java/ssafy/muchung/task/repository/TaskRepository.java
+++ BE/muchung/src/main/java/ssafy/muchung/task/repository/TaskRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a4bbb1eb42daad10fd5b1223807dc4595b09d70ce482085dc1e46739a9390cd
-size 515
+oid sha256:c291c4230b8a07d27e7e633d3224d6bc16d5b2e651966cbe49507840c2fdc299
+size 600
