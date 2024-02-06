76eb609bc2a708dfbde61507453bf705dfaf6374
이수현 <olafminion0116@naver.com>
2024-02-06 13:34:26 +0900

    chore: swagger-ui jwt 토큰 전역 변수로 사용 가능하게 수정
    Former-commit-id: 701c9bfce2a7dd96cb2eb2433497870025662c75

--- BE/muchung/src/main/java/ssafy/muchung/global/config/SwaggerConfig.java
+++ BE/muchung/src/main/java/ssafy/muchung/global/config/SwaggerConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f25aeb630c6123d0b6f5c75c7df532f3452842676a1954ef44f4806f8f387de
-size 2161
+oid sha256:9a648fc4692495418a95d73b2a5f9e6e6a2dff49531208a790d2faa3b3f7371f
+size 3266
