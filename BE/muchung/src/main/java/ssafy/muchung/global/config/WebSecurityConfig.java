5ff9dbc99d3a1d92ada8d0ab9f9d4d2bf06a08df
이수현 <olafminion0116@naver.com>
2024-02-06 09:23:52 +0900

    chore: SpringSecurity 필터 무시 URI 패턴 수정 Former-commit-id: 77bf9d1baf1ca5c4d164ed034657e7918e90951a

--- BE/muchung/src/main/java/ssafy/muchung/global/config/WebSecurityConfig.java
+++ BE/muchung/src/main/java/ssafy/muchung/global/config/WebSecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a14d11ab90096a40d236953da5496ba4fb627a42dca73952b817fb64f1e23078
-size 2231
+oid sha256:9bee6877f9660b440affdcc4f2196e7501e77050e7834afc37c12a9d6082288d
+size 2216
