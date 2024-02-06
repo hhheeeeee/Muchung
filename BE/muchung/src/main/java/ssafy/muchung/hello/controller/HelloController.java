5011d09f7a80a0661782c060633d57fbe033fd43
이수현 <olafminion0116@naver.com>
2024-02-06 10:07:55 +0900

    chore: URI 패턴 변경
    Former-commit-id: c3956ba8c55ee24d24b6730df93d0e1d3072031b

--- BE/muchung/src/main/java/ssafy/muchung/hello/controller/HelloController.java
+++ BE/muchung/src/main/java/ssafy/muchung/hello/controller/HelloController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc2dd8ca5cd8a81c3a63217dd9d9b6d681af2e80bc9ff7d5475a3834372e8041
-size 1275
+oid sha256:2e02c58d35405e9ee6c010eea5f458c1d4b9b2a2d65d3d5a684c66a7147cf651
+size 1279
