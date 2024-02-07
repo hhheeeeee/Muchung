143573e2e02084cb81a21a076f824f2f746f3047
지준영 <bigwkddo@naver.com>
2024-02-07 10:50:04 +0900

    [Feat] (공유피드) card 디자인 적용
    Former-commit-id: ddf1710fd9317fd49848c64ca80628a30753c177

--- FE/src/pages/Feed/Feed.js
+++ FE/src/pages/Feed/Feed.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c4ffb490d9c2290d91b19a2fcaa9d9ed8a52127bca78dcdca9d781e098dda04
-size 10057
+oid sha256:1bfd0467952a0df8dfa4bfcb7bda057b1a5d194e9cea859a27c1b2bec5bc8d10
+size 9213
---
+++ FE/src/pages/Feed/FeedCard.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ef80c96228ec12957fcb9083f45262e9375592fbcde49d04fddc21af59d45d5e
+size 1408
