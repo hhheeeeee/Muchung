0da238d72c12cace371b12fbd07ffb01cea54b69
hhheeeeee <libravickyyy@gmail.com>
2024-02-08 01:00:15 +0900

    [fix] FE : 회원가입 부분 모바일뷰 적용
    Former-commit-id: e7ff189f4aca756d76aea34dd1395e96d74da0f4

---
+++ FE/src/pages/MobileCommute.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1643a802a84431e618e476369c9f625d1bfe5d6f2dab1729400f9d005bf1fdc9
+size 6758
--- FE/src/pages/SignUp/Ncs.js
+++ FE/src/pages/SignUp/Ncs.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec950addf4ca8bedb597e8fd13d03627de6acedc3a67b811a98451e42b240377
-size 4333
+oid sha256:a8069a1cc41498df4c902ade024e2059cf8b22b74cde2f17d8e968408615107e
+size 5903
