c1ee72d87b8ab0a13ad0271eb6c140ca5f4fb80e
이수현 <olafminion0116@naver.com>
2024-02-07 14:34:10 +0900

    [feat] 메일 CRUD API 개발
    Former-commit-id: 5e618ba5ef6a5c090afed622a80d72f808912d4e

---
+++ BE/muchung/src/main/java/ssafy/muchung/mail/controller/MailController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b3dc7ac638e8698eb4f58ceb39dfae1defe462d0d61e907f9f0ce31f83d66ffc
+size 3666
---
+++ BE/muchung/src/main/java/ssafy/muchung/mail/service/MailService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:397045f7aa326289106b39d1feb02b2c0cfd04d6f4affd35c5b3ce66d4777678
+size 3661
