38ac9bd0340f531d84e1d7380886ee32cfa71419
이수현 <olafminion0116@naver.com>
2024-02-07 17:09:12 +0900

    chore: swagger-ui URI path 변경
    Former-commit-id: d9a6842010b5a9e56c62430a72f8fec08aa697f5

--- BE/muchung/src/main/java/ssafy/muchung/global/config/SwaggerConfig.java
+++ BE/muchung/src/main/java/ssafy/muchung/global/config/SwaggerConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a648fc4692495418a95d73b2a5f9e6e6a2dff49531208a790d2faa3b3f7371f
-size 3266
+oid sha256:a61b932a5b897f91534b29f43e84fc020ccfd13139bad50f04205f42ae02869e
+size 3270
