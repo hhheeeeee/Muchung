d8ef8016b7208d4dbe2257f62f9b6550345f5624
이수현 <olafminion0116@naver.com>
2024-02-07 16:05:02 +0900

    feat: 메일 API 전반 isDeleted 확인 후 조회
    Former-commit-id: 2c67c07143299eb0e592134f210f86d39d6b55dc

--- BE/muchung/src/main/java/ssafy/muchung/mail/controller/MailController.java
+++ BE/muchung/src/main/java/ssafy/muchung/mail/controller/MailController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3dc7ac638e8698eb4f58ceb39dfae1defe462d0d61e907f9f0ce31f83d66ffc
-size 3666
+oid sha256:9a72cb3115bb9f6906760a3ae5c9c21fd04c3e780a0a7efea92d4a8d74eac01b
+size 3688
--- BE/muchung/src/main/java/ssafy/muchung/mail/service/MailService.java
+++ BE/muchung/src/main/java/ssafy/muchung/mail/service/MailService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:397045f7aa326289106b39d1feb02b2c0cfd04d6f4affd35c5b3ce66d4777678
-size 3661
+oid sha256:ecf3c9c8c800f0257b582f843f01744d72ccacc1d186665c0b2cf6349fdf8a55
+size 3823
