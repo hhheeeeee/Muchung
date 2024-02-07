778bb2d7338093d8393636081e47809c3b7fd1ff
이수현 <olafminion0116@naver.com>
2024-02-07 16:33:33 +0900

    [BE/feat] 메일 API 일부 조정
    Former-commit-id: f196a88049f9ea619511c96d620b94fe342018f6

--- BE/muchung/src/main/java/ssafy/muchung/mail/controller/MailController.java
+++ BE/muchung/src/main/java/ssafy/muchung/mail/controller/MailController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3dc7ac638e8698eb4f58ceb39dfae1defe462d0d61e907f9f0ce31f83d66ffc
-size 3666
+oid sha256:24e8498700bb11f7247be2c9842e4ab6c0efc9055ebf57f0871e2fcad2cf3966
+size 3995
--- BE/muchung/src/main/java/ssafy/muchung/mail/repository/MailRepository.java
+++ BE/muchung/src/main/java/ssafy/muchung/mail/repository/MailRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:782ad1c20b92be490b3110b65d937bf989941681620e172fe0f7857a466882de
-size 630
+oid sha256:048f51e8e28d0cd75cc23f4f1b467e88c57b2ba460871efe2872d5e22532e54d
+size 843
