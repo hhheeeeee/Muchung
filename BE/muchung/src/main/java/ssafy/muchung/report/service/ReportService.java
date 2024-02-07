de8e95e8067ced4494fc50627413ec09f5929db2
소재열 <devjy0309@gmail.com>
2024-02-07 10:38:46 +0900

    fix: 피드 목록 조회 정렬 기준 reportDate로 변경
    Former-commit-id: 47bf0234d6b85253960d035155dc6a4249174e27

--- BE/muchung/src/main/java/ssafy/muchung/report/service/ReportService.java
+++ BE/muchung/src/main/java/ssafy/muchung/report/service/ReportService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81ffef2db9ab9a374232aa6850b8de3ec9b12202fb9ac5fca6eb3c696211287f
-size 4726
+oid sha256:6d4a139d085d238b92aef36a3d733ea2d50d3af54f473a9e2284f560650edb8a
+size 4727
