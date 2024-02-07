d3d62606babc14c6bbe12d808ce7154881bd64ee
이수현 <olafminion0116@naver.com>
2024-02-07 14:19:29 +0900

    feat: 메일 CRUD API에서 JWT 기반 memberId 추출해서 사용
    Former-commit-id: 0257cd6f508e296ae8206495e6d95e16cc5a45ea

--- BE/muchung/src/main/java/ssafy/muchung/mail/controller/MailController.java
+++ BE/muchung/src/main/java/ssafy/muchung/mail/controller/MailController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a394ed16c6d1aef4b2d4487617f26a09bcac0c3cb71c5d3bab80b48f8d2d1d97
-size 3477
+oid sha256:b3dc7ac638e8698eb4f58ceb39dfae1defe462d0d61e907f9f0ce31f83d66ffc
+size 3666
