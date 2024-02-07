dc94b609639e3fa6ca98fccc65e2916ae11c59b0
소재열 <devjy0309@gmail.com>
2024-02-07 09:16:19 +0900

    fix: main class 배포용으로 수정
    Former-commit-id: eb98db7d226d609ba3690f677c5416e0869aec44

--- BE/muchung/src/main/java/ssafy/muchung/MuchungApplication.java
+++ BE/muchung/src/main/java/ssafy/muchung/MuchungApplication.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6d7dc2a26c2f9756839c51a1f87fdbadf3440493c6de54f994530fbb13bae814
-size 629
+package ssafy.muchung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class MuchungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuchungApplication.class, args);
+    }
+
+}
