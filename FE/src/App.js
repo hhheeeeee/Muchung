8707713a48b132e8acee3575503a19dd6d71c5bc
hhheeeeee <libravickyyy@gmail.com>
2024-02-07 21:50:37 +0900

    [fix] FE : 첫페이지, 로그인, 회원가입 처음까지만 모바일뷰 수정
    Former-commit-id: 481b6e87ccf4a4828bcc82f320473a9926fd39d5

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41f0c0224ff6175b535046ca6ba6cc091f3b1fdbbedcbd802aa94fc7110ffd06
-size 2989
+oid sha256:da4249034c1bc7f9f6b439121bdce4f5d96d8a57507436f9bddac066dca37a5e
+size 3075
