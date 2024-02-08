0172b88ab589733e757b1c1e584de8fd0924e505
지준영 <bigwkddo@naver.com>
2024-02-08 10:53:33 +0900

    [fix] 공유피드 통신문제 해결
    Former-commit-id: 6fff252fe85a8bd033c57be3ee5f18382b158cd0

--- FE/src/pages/NewMainPage/BottomNav.js
+++ FE/src/pages/NewMainPage/BottomNav.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:891f34057562f40e84f662f7f1233eb84c1a7fe352f21ebee2c5a1a2b1065755
+oid sha256:ac8d0bfbee5dd17c0cc11ad15d0a0d26229a6c4b90e62288ba4b8c2b7a02672e
 size 1114
