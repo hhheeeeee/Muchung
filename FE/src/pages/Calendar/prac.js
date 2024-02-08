418ea097be729b3a7cab55a479130c63c1478257
Kim Taehun <kth32105@naver.com>
2024-02-08 17:49:04 +0900

    [feat] FE : 비동기 에러 잡는 중
    Former-commit-id: 97af9210c8aeb5eaaee14196a5596b76f57b1862

--- FE/src/pages/Calendar/prac.js
+++ FE/src/pages/Calendar/prac.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10e715344a545cfaa55a21c97e39e070aaff9f53533fc563766f888d54c249ea
-size 4302
+oid sha256:4d3b91ec32f14643a2dc569374b417bfadea24469d6fd1f6d91e91d712227513
+size 8318
