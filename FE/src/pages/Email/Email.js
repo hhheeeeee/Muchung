abb1e60964ef1a567b29bfab329fe4b24bcf6164
김승희 <libravickyyy@gmail.com>
2024-02-08 14:34:34 +0900

    [feat] FE : PageNotFound 화면 만들기, 받은 이메일 모바일 뷰 레이아웃, 로고 수정
    Former-commit-id: 2e3069c260f06a16c3a73e8355961504da81d1ec

--- FE/src/pages/Email/Email.js
+++ FE/src/pages/Email/Email.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e77b792a4cb143830e8ff1e321feef19acc0adc7cb65e0a778b04d2a9ca2a76
-size 1009
+oid sha256:e991d7b38eede277911db252a320c82ef501d54b364a328ede86c59ee9ab9371
+size 1218
--- FE/src/pages/NewMainPage/MainPage.js
+++ FE/src/pages/NewMainPage/MainPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c006fc753b3ac0995c2e723a86f203bbce21f22fa70938a89211cf6c6d82eb61
-size 858
+oid sha256:0edeeb73f0d1389ae470d3093310df63d688898dad9737fc462e577237a1c793
+size 849
