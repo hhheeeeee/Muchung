59ee0aab0c93297abc45bd87c47ed0df9674a3ac
이수현 <olafminion0116@naver.com>
2024-02-10 02:12:51 +0900

    [FE/fix] base url 변경
    Former-commit-id: b8d156772d6993294b0f5c9675ac8d17bb0b88b7

--- FE/src/api/config.js
+++ FE/src/api/config.js
@@ -1,3 +1,5 @@
 // export const BASE_URL = `http://70.12.246.159:8080`;
 
-export const BASE_URL = `https://i10a307.p.ssafy.io/api`;
+// export const BASE_URL = `https://i10a307.p.ssafy.io/api`;
+
+export const BASE_URL = `http://i10a307.p.ssafy.io:5000`;
