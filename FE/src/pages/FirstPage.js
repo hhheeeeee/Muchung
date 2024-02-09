432ec9c6d00bc3176d0675001a434c28cf94eecc
이수현 <olafminion0116@naver.com>
2024-02-09 23:56:13 +0900

    [fix] FE : 빠진 semicolon 추가
    Former-commit-id: 5fc36b7831492a339d0a5ab8015457e985817212

--- FE/src/pages/FirstPage.js
+++ FE/src/pages/FirstPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bf42290e7221d4eed7ceb1b316a8a6079805aea7b6e43347d2cc7d61cf0af68
-size 4368
+oid sha256:d30a7223834c3be430d958587c553a56999bd1286ee076fcc395099d16efe71f
+size 4369
