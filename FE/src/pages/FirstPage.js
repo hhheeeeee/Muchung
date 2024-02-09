91e847556a2e69fedb552e03d5a0acec816a1c0a
이수현 <olafminion0116@naver.com>
2024-02-10 02:02:07 +0900

    [fix] FE : url 변경
    Former-commit-id: ad2b39b13cdab68ba5a4e3518b41616f050241cb

--- FE/src/pages/FirstPage.js
+++ FE/src/pages/FirstPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:238fc9277afd2c8b26e7a8aff23c4fbadaf6ca9e1872d6e4c4886f434778cbce
-size 4459
+oid sha256:1ce9265ee6cd8a058927e46a9c2380c105b91bc02f23e74d2bd4bd2f9767ff0f
+size 4394
