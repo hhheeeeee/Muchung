9ddf2bedcba3adee0ed4f76e802d886615f3c66c
김이현 <rihyun1126@naver.com>
2024-02-09 23:46:42 +0900

    fix: Redirect Uri 수정 Former-commit-id: e2286f49da4ffb232b84a90f504300c07917800f

--- BE/muchung/src/main/java/ssafy/muchung/global/config/WebSecurityConfig.java
+++ BE/muchung/src/main/java/ssafy/muchung/global/config/WebSecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d5c69e963c33434d8f21f81d9b88f9dda8aca314b9b172caf9935400e33e654
-size 2540
+oid sha256:8df2f369f0120027aa3cade47422295f51f0a0190ed4ee39a4c4db8d3a5896c6
+size 2613
