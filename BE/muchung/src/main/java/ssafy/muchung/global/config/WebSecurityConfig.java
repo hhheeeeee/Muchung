8e8bd11f7b5435ffb3ee80bc1d2e4a875fe0e764
이수현 <olafminion0116@naver.com>
2024-02-10 06:21:55 +0900

    [BE/chore] swagger-ui url 변경
    Former-commit-id: 7269c3c498f3eb8c01344fba6d821841778f9981

--- BE/muchung/src/main/java/ssafy/muchung/global/config/WebSecurityConfig.java
+++ BE/muchung/src/main/java/ssafy/muchung/global/config/WebSecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6db31ffd4fa48f09ecf784dcef2a6f3c5a37078eb58b823b5223c47b466dab5a
-size 2630
+oid sha256:5c30c65e507adfbbc9ac02595b36e2eeb5e81463129d9a34148f966afab68703
+size 2617
