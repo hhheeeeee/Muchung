d7fd7d1cfaf9c5d8a59bc24f2c86ccecd354e4e0
이수현 <olafminion0116@naver.com>
2024-02-10 06:37:54 +0900

    [FE/chore] base url 변경
    Former-commit-id: d5d951d166a661521e1f78e4ad5987175866391e

--- FE/src/pages/FirstPage.js
+++ FE/src/pages/FirstPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa687da69c7e328d905a2baae3083aa79777a8af85fb2ab843a4995ef6da53a5
-size 4498
+oid sha256:ae28f48c3320180591295c52ac23a1b429a5213549dd8359b3ac60fc6ac8270e
+size 4488
