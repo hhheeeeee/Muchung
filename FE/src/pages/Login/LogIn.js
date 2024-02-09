bb45c1035df1f9c15ecb56ef0a3ff4c72fce75a7
이수현 <olafminion0116@naver.com>
2024-02-09 23:48:40 +0900

    [fix] FE : 로그인 관련 수정 시도
    Former-commit-id: 4f583d3416d7316c27f00a2ecf10c9e73bcb3142

--- FE/src/pages/Login/LogIn.js
+++ FE/src/pages/Login/LogIn.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a160ce5fc58bb9a10593d7f40e204e86cac13f526422294cf7c7254b0d7b695
-size 2661
+oid sha256:d2e02f496676400fffcd321b47cf17c9041d3445700bd86df74a93c696f4ee4b
+size 2650
