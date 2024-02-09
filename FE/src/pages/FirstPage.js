01d5c48be4c924a19c056b24c576ba4409c040b9
이수현 <olafminion0116@naver.com>
2024-02-10 00:10:10 +0900

    [fix] FE : 재시도
    Former-commit-id: 206db693ac4e952c1b2d41d655a259014b30fdae

--- FE/src/pages/FirstPage.js
+++ FE/src/pages/FirstPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d30a7223834c3be430d958587c553a56999bd1286ee076fcc395099d16efe71f
-size 4369
+oid sha256:c90fdd50e69b7eb2fffacc064f92b68dd4193855c831c2bfe707c13c778c8931
+size 4375
