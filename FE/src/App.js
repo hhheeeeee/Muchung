7076d5312ccd296918e3f0a8fbbe9a14909d78aa
이수현 <olafminion0116@naver.com>
2024-02-10 00:45:53 +0900

    fix: router 수정 Former-commit-id: d6c23e95801c669cf04a288e098017d82d4d0567

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e4d586439c549124b3d66501a15a7e18b00b8ae849f9c19eed16e2ab2e024f3
-size 3975
+oid sha256:1e374a4614229f13cc9043f3aa1c0ded659dd615e2e6789d4b3daa0769b7c708
+size 4042
