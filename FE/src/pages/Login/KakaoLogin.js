17b22704636ea91ffd3e8cab1e1042ddeadba7d2
hhheeeeee <libravickyyy@gmail.com>
2024-02-10 21:30:58 +0900

    [fix] FE : kakaologin 로딩중 화면 추가
    Former-commit-id: ef6e4f651ec2e1a1923c384ecea0b38d32973e44

--- FE/src/pages/Login/KakaoLogin.js
+++ FE/src/pages/Login/KakaoLogin.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7fc8cd4d464c29de87cee8697dfa7d87c67c25997dd230d5209fe71d052e16f
-size 4598
+oid sha256:3e124cccfb046944dc30858cdb9743c7bf5cdbcd895790482474e5ce07465e4c
+size 4628
