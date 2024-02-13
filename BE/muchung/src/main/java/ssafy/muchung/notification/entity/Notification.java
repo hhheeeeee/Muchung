b19d4ce4ad7e6363348f25eec44c7ea17ff8b9f2
김이현 <rihyun1126@naver.com>
2024-02-13 17:21:47 +0900

    feat: 최근 읽지 않은 알림 5개 조회 & 알림 읽음 처리 기능 구현
    Former-commit-id: 4f74fb630c45afd4d981ab0806babf3e8e3082b5

--- BE/muchung/src/main/java/ssafy/muchung/notification/entity/Notification.java
+++ BE/muchung/src/main/java/ssafy/muchung/notification/entity/Notification.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbbe7b32c3de744c95b1fd2f0d21f0e4aa0e26804eaa1a8dcfeb1bb9c04f2c8e
-size 1014
+oid sha256:98197c0ba25a95466d4d569a8c8dcfc36937c23dc0b66eba8abab2766e99f42f
+size 1044
