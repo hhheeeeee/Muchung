25116f6b04a6441c5e3bb7c7bc312d7f51dfe385
김승희 <libravickyyy@gmail.com>
2024-02-13 17:01:57 +0900

    [feat] FE : 웹 알림 구현 (S10P12A307-329)
    Former-commit-id: 15e09b91ad84d4ec50f60f4a976b1c5dfa6d5823

--- FE/src/pages/NewMainPage/notification.js
+++ FE/src/pages/NewMainPage/notification.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2509f8f7ff14da8afc3bc71a3f9f62854318ff632b9bb8bb228cd65aadbd03ec
-size 8285
+oid sha256:98d878a2b53634fb16335c2a31b5dcdb75cba33e4ff1d9ba3c8e5ce5d47ccc99
+size 3782
