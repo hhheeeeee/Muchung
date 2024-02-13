505d46280086e96e4e56c45d9ce7b127adbbc273
jaeyeol <devjy0309@gmail.com>
2024-02-13 15:35:30 +0900

    hot-fix: 멤버 권한 체크 로직 수정
    Former-commit-id: 8f10ca4f59fc0a0cfa9c6dab41bd4a107f9017d8

--- BE/muchung/src/main/java/ssafy/muchung/comment/service/CommentService.java
+++ BE/muchung/src/main/java/ssafy/muchung/comment/service/CommentService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6427b50b3715683c3c8f77cdc9e9d5283fd6c707310c15e87c6b6d63b2b4ace1
-size 2567
+oid sha256:bc334aa7a1f3b2f650544e75bb400910346908cc04ee4f2e43a0cebd2e4839f9
+size 2313
