6e312878e2481432c98455422cc48e42c5a75554
김승희 <libravickyyy@gmail.com>
2024-02-13 18:13:13 +0900

    [fix] FE : 메인페이지 들어가면 서버 요청에서 최신 알림 5개 받아오기
    Former-commit-id: 5719909d1d8b4b424930ad779145ce147eb88a3d

--- FE/src/pages/NewMainPage/notification.js
+++ FE/src/pages/NewMainPage/notification.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98d878a2b53634fb16335c2a31b5dcdb75cba33e4ff1d9ba3c8e5ce5d47ccc99
-size 3782
+oid sha256:99bafbe06ed076b6fe7adbc32de1b786339003ee277378b0478fb02924eb4fd4
+size 3853
