a5c6fd39ac306a5f1e93314ec09a0e616dda14d1
김이현 <rihyun1126@naver.com>
2024-02-13 12:23:20 +0900

    [BE] fix: 회원 탈퇴 기능 수정
    Former-commit-id: b69ee43a63bfac4ca06c6798076646fffdb5fdd4

--- BE/muchung/src/main/java/ssafy/muchung/member/service/MemberService.java
+++ BE/muchung/src/main/java/ssafy/muchung/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bff573c940c69b9c9db7486dbf910d2dff379d0caaa906d6ec776d9584e345c8
-size 4182
+oid sha256:236ef2ccee23cf95c8aae9c0dd8b0ad70ca22fcec96023a0513e9c80c6d06ad0
+size 4189
