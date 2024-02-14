fd625df0baf797e28ccff582310a3176dc3ff597
jaeyeol <devjy0309@gmail.com>
2024-02-14 13:33:45 +0900

    hot-fix: 멤버 부서 데이터 전달 하도록 변경
    Former-commit-id: d8f5732040757ec2f9a5e0cf4c9a19bd0ca706e6

--- BE/muchung/src/main/java/ssafy/muchung/member/entity/Member.java
+++ BE/muchung/src/main/java/ssafy/muchung/member/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff9951e1dbcbd29a3a4fb1675fb2358efe65af53ea4df7a94cf2e3a7c0c43c59
-size 1225
+oid sha256:4df4a601ef76d4387fd450b29067c230a61f023f61cf62a160e031034ed5f4e3
+size 1226
