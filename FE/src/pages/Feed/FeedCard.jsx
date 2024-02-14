260103156768198a9622fd792659f9fe250a725c
김이현 <rihyun1126@naver.com>
2024-02-14 18:08:29 +0900

    [fix] FE : Feed 디자인 수정
    Former-commit-id: 1c37ae2b322bac1fa77bf7d11e8be7f866083056

--- FE/src/pages/Feed/FeedCard.jsx
+++ FE/src/pages/Feed/FeedCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb93e55db075cacfcd2387ea253058e1d7db807cd97f0f6945a54a844d5e087b
-size 1095
+oid sha256:93ebf7b375fceab1a2ff0addff160ce67d7f9e7f0a34ef4bd2aafdc06f8649ae
+size 1125
