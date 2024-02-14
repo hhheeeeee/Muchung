243d68a05bda6ac1d27f02d00042b9cd7f65923c
jaeyeol <devjy0309@gmail.com>
2024-02-14 09:51:04 +0900

    fix: fe/develop merge
    Former-commit-id: d144bdccf73d45d209fb16256c9eeef935df65a2

--- FE/src/pages/NewMainPage/MainTodo.js
+++ FE/src/pages/NewMainPage/MainTodo.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:858147cebe7286d35296a48d563c7523db18b741b30d92f01572aefc11500541
-size 20240
+oid sha256:496782aae88de151dafa752ccadecb7f9c1ba24da4f1eea2b3d5cf12ff3e0eef
+size 17833
