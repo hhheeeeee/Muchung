118f1823146b9c185458cab5c37820f226cb9b1c
jaeyeol <devjy0309@gmail.com>
2024-02-14 13:49:12 +0900

    hot-fix: 멤버 부서 가져오지 못하는 오류 수정
    Former-commit-id: a705f649ab774c2eb5b0cd867a1b475ccb688de8

--- BE/muchung/src/main/java/ssafy/muchung/report/dto/response/ReportsFeed.java
+++ BE/muchung/src/main/java/ssafy/muchung/report/dto/response/ReportsFeed.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd511ae16da6f07fbe096ab7abc726a21f1d49102e8c7d4c5e00cb6e9388a786
-size 1162
+oid sha256:9237b52716dc2f0b20cfa6bf847ec81bd331cacb278a4dafdbfbd8a02818ed26
+size 1213
