2eb93073eceff5c5b079ac489fb4c989dbd6f768
Kim Taehun <kth32105@naver.com>
2024-02-14 09:44:11 +0900

    [feat] FE : release 업데이트
    Former-commit-id: 29f8318da94f52a96096a94b8a57c9b25985ae37

--- FE/src/pages/NewMainPage/MainTodo.js
+++ FE/src/pages/NewMainPage/MainTodo.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef04bf12c43cfa5a8d8fe82f7128b8b4eb56dfa8ffaf82bbae2aaa4eb56d85a4
-size 16352
+oid sha256:bd91dd5cddc121ed2d37f2f964046d864a06708abc7b113376f764f99c9b7891
+size 16912
