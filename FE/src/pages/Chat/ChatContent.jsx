8a63626140f1fd6527599024bc3c8b680218283b
이수현 <olafminion0116@naver.com>
2024-02-14 18:12:27 +0900

    [FE/feat] 챗GPT 요청 로직 변경: 질문 3개 생성(, 요약, 추천) 시에만 호출
    Former-commit-id: c17529b5aeba7df020e3114189af7358e718261f

--- FE/src/pages/Chat/ChatContent.jsx
+++ FE/src/pages/Chat/ChatContent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67f06487808f30e2bd5498b38dd9d83f24fe125004ee1a54f219e1a5bba64d19
-size 12915
+oid sha256:33be924a37e457bbe184eab7e784d8e0d950e02fa26f280cd4188f6519fd9952
+size 14259
