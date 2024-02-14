989795ca91858758d73c61ca569c9b75eb1109b3
jaeyeol <devjy0309@gmail.com>
2024-02-14 13:56:23 +0900

    멤버 이름 받아와서 출력 및 보고서 에러 수정
    Former-commit-id: 7df320e549350271224bc9110af74e650d67dbfb

--- FE/src/pages/Calendar/prac.js
+++ FE/src/pages/Calendar/prac.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e38c5aeaef0de38838ee9a38b00b0bfbfda85a9711fd05114dc6370b41312572
-size 7955
+oid sha256:988af0184f8df26e07e0e942c14e8c7992d51d4942240fa07031025d5ef2aa0d
+size 9215
