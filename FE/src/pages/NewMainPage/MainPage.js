f6bdc91e6bf35c0c634510ede356705b6a2867ac
김승희 <libravickyyy@gmail.com>
2024-02-14 17:45:47 +0900

    [feat] FE : 회원 탈퇴 버튼 추가
    Former-commit-id: 0a0203b734483ccd0fa6892175986734e44c9b01

--- FE/src/pages/NewMainPage/MainPage.js
+++ FE/src/pages/NewMainPage/MainPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95150f3571cdcc7278f9fb7f8f4c12eef8074ceb7d7ca792b85f10deeb7ff1ec
-size 846
+oid sha256:4a3f759160fb0814f30cf589662f988c8deba96e6c5d2e0f90a52c8aa779d1b6
+size 672
