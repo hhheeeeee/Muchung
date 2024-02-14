1ad9ba184b77e18afdf79db5ce1845c97d3cb3ce
jaeyeol <devjy0309@gmail.com>
2024-02-14 13:43:56 +0900

    hot-fix: 멤버 부서 펫치 타입 수정
    Former-commit-id: 83bbcc507e1ba1c92b9da56efec3c0ad85200805

--- BE/muchung/src/main/java/ssafy/muchung/member/entity/Member.java
+++ BE/muchung/src/main/java/ssafy/muchung/member/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4df4a601ef76d4387fd450b29067c230a61f023f61cf62a160e031034ed5f4e3
-size 1226
+oid sha256:781fd51eed2d2e8eb1f8b429995105e66eb7106ddb33502d292cfead0101aa38
+size 1167
