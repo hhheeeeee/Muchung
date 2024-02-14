e7ffcf346ac3feacbfb91ce4ef45311d3cb96844
이수현 <olafminion0116@naver.com>
2024-02-15 04:39:33 +0900

    [FE/feat] 챗GPT 로직 변경, API 모듈화, 답변 로딩 시 input disabled
    Former-commit-id: 74d0a74077889b1d34a13ded8b7f3bf29961865c

---
+++ FE/src/pages/Chat/ChatApi.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:81365e217d80976b0fb9f16587e8d22569122a46786f0e99cd53a2616eb1f350
+size 1999
--- FE/src/pages/Chat/ChatContent.jsx
+++ FE/src/pages/Chat/ChatContent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33be924a37e457bbe184eab7e784d8e0d950e02fa26f280cd4188f6519fd9952
-size 14259
+oid sha256:efe775eb3b6d185e08ac32f61833d8ff91a08f8fb003b8079f4b5ee780091ece
+size 12328
