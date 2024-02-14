c3356a7458b1138cffc6f193353a0977f5998464
김이현 <rihyun1126@naver.com>
2024-02-14 09:07:47 +0900

    fix: 로그인 시 id 전달하도록 수정
    Former-commit-id: ed5a05f95e2d6eeb35d0ce3e1f59fa6b43c42508

--- BE/muchung/src/main/java/ssafy/muchung/member/service/MemberService.java
+++ BE/muchung/src/main/java/ssafy/muchung/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:236ef2ccee23cf95c8aae9c0dd8b0ad70ca22fcec96023a0513e9c80c6d06ad0
-size 4189
+oid sha256:af6e4cfc5e9aae3b0bd231000f89025047deb023e834aaedbb6f6bd07be9b74a
+size 4212
