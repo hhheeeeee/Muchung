4331ac77807373a3a72b700c5fa5ec5b4e89790c
jaeyeol <devjy0309@gmail.com>
2024-02-15 17:43:00 +0900

    feat: 업무 보고 버튼 링크 연결
    Former-commit-id: 784730be5f22d331181f693559dbbddc318702eb

--- FE/src/pages/NewMainPage/MissionClear.js
+++ FE/src/pages/NewMainPage/MissionClear.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62b5f61eb17534c793e9a46064fcad82f00265277841cdca9b4185e599adac9e
-size 1271
+oid sha256:5afeaff82181e1cac0c307f7d8873ea62c0399cfcca622bc0da2fda426dc8866
+size 1474
