4c7f90c61ae9397bc4b7c249b6aa857bbf99da50
hhheeeeee <libravickyyy@gmail.com>
2024-02-15 22:39:43 +0900

    [feat] FE : 폰트 적용
    Former-commit-id: 73a7bc09532ee22405805a773cfae73730559be0

--- FE/src/themes/reset.js
+++ FE/src/themes/reset.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b32b63bedc03abb2bd7df293479ac00c5a5c73aa1b8fca57715ba977c37d733
-size 1506
+oid sha256:92eab22d0d829be01451cbdd7487c8b42237f334b8d2a8b870cb56412d4f053e
+size 1682
