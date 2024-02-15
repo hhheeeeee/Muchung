f59ee8ffd35daadc442e3880ce5276b2cbfd3d78
jaeyeol <devjy0309@gmail.com>
2024-02-16 04:35:16 +0900

    닫기 버튼 제거
    Former-commit-id: 9f734fd67b0a9493d1b532458675951528bc3649

--- FE/src/pages/NewMainPage/RecommendButton.js
+++ FE/src/pages/NewMainPage/RecommendButton.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:955f33185c2ce725fc4f3ff82b3a47e452c16c21176b38c2aaca3b749447e8e0
-size 3708
+oid sha256:f6442414003a4fef48dd53804b2314b7edb8c4af72b0a258cb98a5b765ed2e75
+size 3716
