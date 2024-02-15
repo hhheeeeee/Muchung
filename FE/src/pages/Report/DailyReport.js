a11f49c206410863eb44cf81e33e9151b93684dc
Kim Taehun <kth32105@naver.com>
2024-02-15 10:15:33 +0900

    [feat] FE : 리포트 기능 완
    Former-commit-id: a1b2e251a3b6db798ed76240fc2c314979448370

--- FE/src/pages/Report/DailyReport.js
+++ FE/src/pages/Report/DailyReport.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4e0f4187ae4fac0eac54e3e72eaa41268656f69e256dd4430ea132393205f21
-size 12410
+oid sha256:70e3b4e2943c87856297930f3ed4a4816437bcbb6b5f54352bbe8feed22efb2b
+size 14456
