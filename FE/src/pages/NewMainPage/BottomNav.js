a3cf26b280e1479e45c58688526c2df8f0c17bd7
김승희 <libravickyyy@gmail.com>
2024-02-15 09:49:28 +0900

    [fix] FE : 회원가입 NCS => Final 부분 버튼을 누르지 않아도 그냥 넘어가는 오류 해결
    Former-commit-id: 66e238d0c112655365ba49084159f419b3cbc4d1

--- FE/src/pages/NewMainPage/BottomNav.js
+++ FE/src/pages/NewMainPage/BottomNav.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df6fad1bfdb080a7bb5cf93ed1ce25ae8fc227116b115e36d20901e642be9187
-size 1452
+oid sha256:e6a93d9b33cad839beb54e0a22b9ed8b4782e93995836d6bb74447b0cbd470dd
+size 1526
--- FE/src/pages/SignUp/Ncs.js
+++ FE/src/pages/SignUp/Ncs.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:973b64043f762600ee07020fea56491462f4cedb9f88caff06a3d8be0ded5908
-size 5782
+oid sha256:b934b59dcc064ea3fe8a0ac1503d66fbaecfa7dd24ad180085796624230d92be
+size 7641
