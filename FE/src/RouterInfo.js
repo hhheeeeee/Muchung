2ea9e1761eaccfcd1b562fef060e348fefad0248
jaeyeol <devjy0309@gmail.com>
2024-02-16 00:16:44 +0900

    hot-fix: 라우터 수정
    Former-commit-id: 4474f451c8c0296f04401f5d6f7903f46ea24f70

--- FE/src/RouterInfo.js
+++ FE/src/RouterInfo.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2904dfcb67491d161bc3f89ee55799635d9d779c01bb65e95502dfb741586c16
-size 3903
+oid sha256:e6180f15a68d9ca2beff39b00428ea77b57f86c3e67763acec395f9bdf3932d2
+size 3911
