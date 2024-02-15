54105b645924bafb891420a64a74f1163d1f269b
김승희 <libravickyyy@gmail.com>
2024-02-15 17:42:13 +0900

    [fix] FE : 알림 읽은 개수 처리
    Former-commit-id: 0fd9d31eb083398b919787346638237f5e7c144e

--- FE/src/pages/NewMainPage/notification.js
+++ FE/src/pages/NewMainPage/notification.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d15414d3375eee4004fd003c3ac16fcc096e38aec2e1cd5484153c3bb07f5d96
-size 6089
+oid sha256:92ee7a1fa48bd510e151915b7ded53a73c7e42b5b739f7fb525537ccddfb3b26
+size 7469
