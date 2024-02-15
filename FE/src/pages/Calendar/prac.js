2f81fdde7f01d8e58096212df9e2a89d410ac41c
김승희 <libravickyyy@gmail.com>
2024-02-15 16:22:55 +0900

    [fix] FE : calendar border, header 높이 수정
    Former-commit-id: b251f961512a0a57d4d8c4ebcf5b2131e6073c76

--- FE/src/pages/Calendar/prac.js
+++ FE/src/pages/Calendar/prac.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd825a959f5f72a821e0f57cb63fad80e3f0f673f2d54ddabcda191a776f4462
-size 7894
+oid sha256:7c1673a22f2da57e81f780b8df2a7e04d4ab16fc8f6b31835387308bb7026d7f
+size 7792
