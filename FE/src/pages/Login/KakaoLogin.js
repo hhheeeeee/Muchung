160173cb88a0df0365dfefa58af779595674a723
이수현 <olafminion0116@naver.com>
2024-02-15 17:58:01 +0900

    feat: 로컬 스토리지에 유저 프로필사진URL, 부서 이름 저장
    Former-commit-id: a69edccff9a0625206cf78296fbbad192134082e

--- FE/src/pages/Login/KakaoLogin.js
+++ FE/src/pages/Login/KakaoLogin.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:202e18325016e2c1b2d4c73804117fe50e4d5194a95c8635cceb37e28000a572
-size 5353
+oid sha256:0c5b818dab53d8e2cf79d34ff304f6e23265ba95715a9cd29711a41cf433e775
+size 5491
