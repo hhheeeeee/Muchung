1fc2a3961856e19a33293953d21851674ceeda8f
hhheeeeee <libravickyyy@gmail.com>
2024-02-15 22:26:03 +0900

    [fix] FE : 최상단 maxwidth maxheight 지정
    Former-commit-id: dda6afafe37f0e5aa57c160113f83412842f79aa

--- FE/src/pages/NewMainPage/BottomNav.js
+++ FE/src/pages/NewMainPage/BottomNav.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f1b3b9b196a5a256d5be6990dfc3fd830c1b5536b3bb00ee79f4515f11f9175
-size 1521
+oid sha256:5e06b703f681b03aa41d923a105482625b8f9032d5fc2124c71bb162e44010cf
+size 1575
--- FE/src/themes/reset.js
+++ FE/src/themes/reset.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70055cfbd2e1c0814f971e3dae542e8b3c719fe9f9ca4d48a4367b25c9dc7c55
-size 1324
+oid sha256:1b32b63bedc03abb2bd7df293479ac00c5a5c73aa1b8fca57715ba977c37d733
+size 1506
