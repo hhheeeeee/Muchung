1e08d24651540e2c3b4b8923e3c267534d74ef6b
jaeyeol <devjy0309@gmail.com>
2024-02-15 16:36:16 +0900

    hot-fix: 추천 업무 API : 랜덤 로직 추가
    Former-commit-id: 6ebaf8afc8a9a8dfa6483e5522cd16bfc53be97e

--- BE/muchung/src/main/java/ssafy/muchung/recommend/service/RecommendService.java
+++ BE/muchung/src/main/java/ssafy/muchung/recommend/service/RecommendService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cb363ec82f62db217fc0ae5fbab3b449e9a65869fd5d57725d893380e63ad58
-size 2369
+oid sha256:e3172671902582c6ad15b40728e1cc53cb26f755f37268bd62d96cd91ba4a099
+size 2328
