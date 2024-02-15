b804ef19441ac833c77c139920e8df57a80b6bb7
jaeyeol <devjy0309@gmail.com>
2024-02-16 04:25:16 +0900

    feat: 이메일 하단 내용 수정
    Former-commit-id: be9b8122effd47a4d643219d4038f5d88cb3df4f

--- FE/src/pages/Email/CreateEmail.js
+++ FE/src/pages/Email/CreateEmail.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19569df8f32b31a5d5846f3ac3d11c097dfd7508ca1a4509bc4808ff64662a7e
-size 6137
+oid sha256:168c20629e202a8ea54f0f881d7f581a628dba9f52d0bf1ad0a1f81d52e1a01b
+size 6074
