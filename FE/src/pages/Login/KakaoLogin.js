4f48ed3909da87cfc1f6787f09207beb2ecf4243
jaeyeol <devjy0309@gmail.com>
2024-02-15 09:02:49 +0900

    fix: import error 수정
    Former-commit-id: 43bf50de47eee83b1a6d61f4976ada0fce1f6053

--- FE/src/pages/Login/KakaoLogin.js
+++ FE/src/pages/Login/KakaoLogin.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ac9abd6d5c8f7349d42483170ca7a21dadf3b53b884811b14d3102957bd0f22
-size 5395
+oid sha256:929838673f1489730ba44747b1996a30243faf7ccfbfb5e9ba277d961fbecfcd
+size 5338
