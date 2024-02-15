ff32aefad9c340c797d76e068ad76d2633828f70
hhheeeeee <libravickyyy@gmail.com>
2024-02-15 21:28:39 +0900

    [fix] FE : 알림 오류 고침
    Former-commit-id: 7a354c4259bd0fb35fd1050b866d33eb203a0f00

--- FE/src/pages/NewMainPage/notification.js
+++ FE/src/pages/NewMainPage/notification.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92ee7a1fa48bd510e151915b7ded53a73c7e42b5b739f7fb525537ccddfb3b26
-size 7469
+oid sha256:ac0b3f07346c3d6303850b5be0bd3c0fe0f1d5d20a9b66a682eb2a1a928d4da1
+size 7600
